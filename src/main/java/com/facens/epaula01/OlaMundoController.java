4f19719a51373d68c0d16031ff9e2ae412757ec2
Lucas Mendes Nemeth <125515363+luc0428@users.noreply.github.com>
2026-03-12 21:14:04 -0300

    Add files via upload

---
+++ src/main/java/com/facens/epaula01/OlaMundoController.java
@@ -0,0 +1,13 @@
+package com.facens.epaula01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OlaMundoController {
+
+    @GetMapping("/olaMundo")
+    public String ola() {
+        return "Bem-vindo à disciplina de Desenvolvimento Web Back-End 🚀";
+    }
+}
---
+++ src/test/java/com/facens/epaula01/Epaula01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facens.epaula01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Epaula01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
